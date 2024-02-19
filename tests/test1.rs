70c792361ac22ec358bb58bc07c3d44c2f8be452
unknown <rohitrscode@gmail.com>
2024-02-19 18:15:18 +0530

    initial commit

---
+++ tests/test1.rs
@@ -0,0 +1,23 @@
+use assert_cmd::Command;
+use predicates::prelude::*;
+
+
+type Test = Result<(), Box< dyn std::error::Error>>;
+
+#[test]
+fn check_no_args() -> Test {
+    let mut cmd = Command::cargo_bin("echoc")?;
+    cmd.assert()
+        .failure()
+        .stderr(predicates::str::contains("USAGE"));
+    Ok(())
+}
+
+#[test]
+fn runs() -> Test {
+    let mut cmd = Command::cargo_bin("echoc")?;
+    cmd.args(["Hello,World!","Hello","This World Is Amazing!"])
+        .assert()
+        .success();
+    Ok(())
+}
